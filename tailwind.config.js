aa2cea7365e1850c32d33ccfe09946123a35aa20
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2026-01-02 05:47:17 +0100

    Adjusted the Architecture & Setup tailwindcss & Started by creating the Admin side

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#f49d25",
+        "primary-dark": "#d68310",
+        "background-light": "#f8f7f5",
+        "background-dark": "#221a10",
+        "surface-light": "#ffffff",
+        "surface-dark": "#2d2418",
+        "text-main": "#1c160d",
+        "text-muted": "#9c7a49",
+      },
+      fontFamily: {
+        "display": ["Inter", "sans-serif"]
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
